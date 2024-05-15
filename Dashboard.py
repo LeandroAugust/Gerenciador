4f9fac13383d585d37dcf1b7b8e34a2bbcad53b2
JeniiAntunes <113281058+JeniiAntunes@users.noreply.github.com>
2024-05-15 20:15:39 -0300

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -12,16 +12,15 @@ df=df.sort_values("Date")
 
 #Com uma visão mensal
 
-df["Month"] = df["Date"].apply(lambda x: str(x.year)+"-"+ str(x,month))
+df["Month"] = df["Date"].apply(lambda x: str(x.year)+"-"+ str(x,month).zfill(2))
 month = st.sidebar.selectbox("Mês",df["Month"].unique())
 
 #Filtro do pandas para meses e anos
 df_filtered = df[df["Month"] == month]
-df_filtered
 
 #Caixas
 col1, col2 = st.columns(2)
-col3, col4, col5 = st.columns(2)
+col3, col4, col5 = st.columns(3)
 
 #Graficos
 
@@ -30,14 +29,14 @@ fig_date = px.bar(df_filtered, x="Date", y="Total", color="City", title="Faturam
 col1.plotly_chart(fig_date)
 
 # Grafico de faturamento por tipo de produto
-fig_prod = px.bar(df_filtered, x="Date", y="Product line", color="City", title="Faturamento por produto",
+fig_prod = px.bar(df_filtered, x="Product line", y="Total", color="City", title="Faturamento por produto",
                   orientation="h")
 col2.plotly_chart(fig_prod)
 
 # Grafico de contribuição por filial
-ity_total = df_filtered.groupby("City")[["Total"]].sum().reset_index()
-fig_city = px.bar(df_filtered, x="City", y="Product line", title="Faturamento por filial",
-                  orientation="h")
+city_total = df_filtered.groupby("City")[["Total"]].sum().reset_index()
+fig_city = px.bar(city_total, x="City", y="total", title="Faturamento por filial",
+                  orientation="v")
 col3.plotly_chart(fig_city)
 
 # Grafico de faturamento por tipo de pagamento
@@ -47,8 +46,8 @@ fig_kind = px.pie(df_filtered, values="Total", names="Payment",
 col4.plotly_chart(fig_kind)
 
 #Avaliação media por cidade
-city_total = df_filtered.groupby("City")[["Rating"]].mean().reset_index()
-fig_rating = px.bar(df_filtered, y="Rating", x="City",
+city_rating = df_filtered.groupby("City")[["Rating"]].mean().reset_index()
+fig_rating = px.bar(city_rating, x="city", y="Rating",
                   title="Avaliação")
 col5.plotly_chart(fig_rating, use_container_width=True)
 
