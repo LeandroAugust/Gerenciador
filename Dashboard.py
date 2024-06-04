b42a9303bfc99347664e1d8d1a36387e2ad7fce5
FelipeKRUGweil <106886806+FelipeKRUGweil@users.noreply.github.com>
2024-06-04 14:25:47 -0300

    Add files via upload

--- Dashboard.py
+++ Dashboard.py
@@ -1,56 +1,31 @@
-import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-st.set_page_config(layout="wide")
+#def grafico():
+    
+
+    #Selecionando o arquivo
+tab_df = pd.read_excel("tabela.ods")
+df = px.data.tips
+
+    #seleçao de coluna
+Data_df = tab_df["Data"]
+instagram_df = tab_df["Instagram"]
+tiktok_df = tab_df["tiktok"]
+trabalhando_df = tab_df["trabalhando"]
+Total_df = tab_df["Total"]
+
+seletor = Total_df
+#-------
+
+#grafico
+fig_graf = px.bar (tab_df, x = Data_df, y= seletor)
+Data_df = px.data.medals_long()
 
 
-#df= pd.read_csv(se for outro tipo de planilha altera("nome da planilha com os dados", sep"separação", decimal )
-df= pd.read_csv("supermarket_sales.csv", sep=";", decimal=",")
-df["Date"] = pd.to_datetime(df["Date"])
-df=df.sort_values("Date")
+mostra = fig_graf.show (), 
 
-#Com uma visão mensal
-
-df["Month"] = df["Date"].apply(lambda x: str(x.year)+"-"+ str(x,month).zfill(2))
-month = st.sidebar.selectbox("Mês",df["Month"].unique())
-
-#Filtro do pandas para meses e anos
-df_filtered = df[df["Month"] == month]
-
-#Caixas
-col1, col2 = st.columns(2)
-col3, col4, col5 = st.columns(3)
-
-#Graficos
-
-#Grafico de faturamento por dia pelo mês selecionado e por filial 
-fig_date = px.bar(df_filtered, x="Date", y="Total", color="City", title="Faturamento por dia")
-col1.plotly_chart(fig_date)
-
-# Grafico de faturamento por tipo de produto
-fig_prod = px.bar(df_filtered, x="Product line", y="Total", color="City", title="Faturamento por produto",
-                  orientation="h")
-col2.plotly_chart(fig_prod)
-
-# Grafico de contribuição por filial
-city_total = df_filtered.groupby("City")[["Total"]].sum().reset_index()
-fig_city = px.bar(city_total, x="City", y="total", title="Faturamento por filial",
-                  orientation="v")
-col3.plotly_chart(fig_city)
-
-# Grafico de faturamento por tipo de pagamento
-
-fig_kind = px.pie(df_filtered, values="Total", names="Payment",
-                  title="Faturamento por tipo de pagamento")
-col4.plotly_chart(fig_kind)
-
-#Avaliação media por cidade
-city_rating = df_filtered.groupby("City")[["Rating"]].mean().reset_index()
-fig_rating = px.bar(city_rating, x="city", y="Rating",
-                  title="Avaliação")
-col5.plotly_chart(fig_rating, use_container_width=True)
-
+#-------
 
 
 
